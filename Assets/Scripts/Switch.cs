d4507474ab4817612aefaa31499885706e378e8e
Elias999-creator <71507603+Elias999-creator@users.noreply.github.com>
2022-03-14 22:43:12 -0400

    Complete Lights logic

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
+    public GameObject up;
+    public GameObject on;
+    public bool isOn;
+    public bool isUp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        on.SetActive(isOn);
+        up.SetActive(isUp);
+        if (isOn)
+        {
+            Main.Instance.SwitchChange(1);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseUp()
+    {
+        isUp = !isUp;
+        isOn = !isOn;
+        on.SetActive(isOn);
+        up.SetActive(isUp);
+
+        if (isOn)
+        {
+            Main.Instance.SwitchChange(1);
+        }
+        else
+        {
+            Main.Instance.SwitchChange(-1);
+        }
     }
 }
