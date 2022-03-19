266b0db7870360386cb925e5121e6fa2a02301a7
Elias999-creator <71507603+Elias999-creator@users.noreply.github.com>
2022-03-18 20:05:44 -0400

    Import Sun_Temple assets

--- Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
+++ Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
@@ -2,132 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharController_Motor : MonoBehaviour {
+namespace SunTemple{
 
-	private float speed = 10.0f;
-	public float walkSpeed;
-	public float sprintSpeed;
-	public float crouchSpeed;
-	public float crouchYScale;
-	private float startYscale;
-	public float sensitivity = 30.0f;
-	public float WaterHeight = 15.5f;
-	CharacterController character;
-	public GameObject cam;
-	public KeyCode sprintKey = KeyCode.LeftShift;
-	public KeyCode crouchKey = KeyCode.C;
-	float moveFB, moveLR;
-	float rotX, rotY;
-	public bool webGLRightClickRotation = true;
-	float gravity = -9.8f;
 
-	public MovementState state;
+	public class CharController_Motor : MonoBehaviour {
 
-	Rigidbody rb;
+		public float speed = 10.0f;
+		public float sensitivity = 60.0f;
+		CharacterController character;
+		public GameObject cam;
+		float moveFB, moveLR;	
+		float rotHorizontal, rotVertical;
+		public bool webGLRightClickRotation = true;
+		float gravity = -9.8f;
 
-	public enum MovementState
-    {
-		walking,
-		sprinting,
-		crouching,
-		air
-    }
+		//string debugText;
 
-	void Start(){
-		//LockCursor ();
-		character = GetComponent<CharacterController> ();
-		if (Application.isEditor) {
+
+		void Start(){
+
+			character = GetComponent<CharacterController> ();
+
 			webGLRightClickRotation = false;
-			sensitivity = sensitivity * 1.5f;
-		}
 
-		startYscale = transform.localScale.y;
-	}
-
-	private void MyInput()
-    {
-		if(Input.GetKeyDown(crouchKey))
-        {
-			transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
-			rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
-        }
-
-		if(Input.GetKeyUp(crouchKey))
-        {
-			transform.localScale = new Vector3(transform.localScale.x, startYscale, transform.localScale.z);
-		}
-    }
-
-	private void StateHandler()
-    {
-		if(Input.GetKey(crouchKey))
-        {
-			state = MovementState.crouching;
-			speed = crouchSpeed;
-        }
-
-		if(Input.GetKey(sprintKey))
-        {
-			state = MovementState.sprinting;
-			speed = sprintSpeed;
-        }
-
-		else
-        {
-			state = MovementState.walking;
-			speed = walkSpeed;
-        }
-
-
-    }
-
-	void CheckForWaterHeight(){
-		if (transform.position.y < WaterHeight) {
-			gravity = 0f;			
-		} else {
-			gravity = -9.8f;
-		}
-	}
-
-
-
-	void Update(){
-		moveFB = Input.GetAxis ("Horizontal") * speed;
-		moveLR = Input.GetAxis ("Vertical") * speed;
-
-		rotX = Input.GetAxis ("Mouse X") * sensitivity;
-		rotY = Input.GetAxis ("Mouse Y") * sensitivity;
-
-		//rotX = Input.GetKey (KeyCode.Joystick1Button4);
-		//rotY = Input.GetKey (KeyCode.Joystick1Button5);
-
-		MyInput();
-		CheckForWaterHeight ();
-		StateHandler();
-
-
-		Vector3 movement = new Vector3 (moveFB, gravity, moveLR);
-
-
-
-		if (webGLRightClickRotation) {
-			if (Input.GetKey (KeyCode.Mouse0)) {
-				CameraRotation (cam, rotX, rotY);
+			if (Application.platform == RuntimePlatform.WebGLPlayer) {
+				webGLRightClickRotation = true;
+				sensitivity = sensitivity * 1.5f;
 			}
-		} else if (!webGLRightClickRotation) {
-			CameraRotation (cam, rotX, rotY);
+
+
 		}
 
-		movement = transform.rotation * movement;
-		character.Move (movement * Time.deltaTime);
+
+
+
+
+		void FixedUpdate(){
+			moveFB = Input.GetAxis ("Horizontal") * speed;
+			moveLR = Input.GetAxis ("Vertical") * speed;
+
+			rotHorizontal = Input.GetAxisRaw ("Mouse X") * sensitivity;
+			rotVertical = Input.GetAxisRaw ("Mouse Y") * sensitivity;
+
+
+			Vector3 movement = new Vector3 (moveFB, gravity, moveLR);
+
+
+			if (webGLRightClickRotation) {
+				if (Input.GetKey (KeyCode.Mouse0)) {
+					CameraRotation (cam, rotHorizontal, rotVertical);
+				}
+			} else if (!webGLRightClickRotation) {
+				CameraRotation (cam, rotHorizontal, rotVertical);
+			}
+
+			movement = transform.rotation * movement;
+			character.Move (movement * Time.fixedDeltaTime);
+		}
+
+
+
+
+
+	
+
+		void CameraRotation(GameObject cam, float rotHorizontal, float rotVertical){	
+
+			transform.Rotate (0, rotHorizontal * Time.fixedDeltaTime, 0);
+			cam.transform.Rotate (-rotVertical * Time.fixedDeltaTime, 0, 0);
+
+
+
+			if (Mathf.Abs (cam.transform.localRotation.x) > 0.7) {
+
+				float clamped = 0.7f * Mathf.Sign (cam.transform.localRotation.x); 
+
+				Quaternion adjustedRotation = new Quaternion (clamped, cam.transform.localRotation.y, cam.transform.localRotation.z, cam.transform.localRotation.w);
+				cam.transform.localRotation = adjustedRotation;
+			}
+
+
+		}
+
+
+
+
 	}
-
-
-	void CameraRotation(GameObject cam, float rotX, float rotY){		
-		transform.Rotate (0, rotX * Time.deltaTime, 0);
-		cam.transform.Rotate (-rotY * Time.deltaTime, 0, 0);
-	}
-
 
 
 
